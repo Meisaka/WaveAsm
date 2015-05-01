bd17694b7b022abc181ebe5170b5c69a1b74c57a
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2015-05-01 09:57:22 +0900

    Made the lexer lex things in lexical ways.
    it uses smoke, mirrors and magic tables.

--- libwaveasm/lexer.c
+++ libwaveasm/lexer.c
@@ -24,7 +24,7 @@
 #define TKWS 1
 #define TKHASH 2
 #define TKL 3
-#define TKN 4
+#define TKDEC 4
 #define TKBANG 5
 #define TKDQ 6
 #define TKDS 7
@@ -45,40 +45,176 @@
 #define TKGT 22
 #define TKEQ 23
 #define TKEOL 24
+#define TKCOMMENT 25
+#define TKHEX 26
+#define TKOCT 27
+#define TKBIN 28
 
-static int sttab[256] = {
+// these tables determine which mode to switch to based on charactor input.
+static int sttab[][256] = {
+{
 	0,0,0,0,0,0,0,0,0,0,TKEOL,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	TKWS,TKBANG,TKDQ,TKHASH,TKDS,TKPCS,TKAMP,TKSQ,
 	TKLP,TKRP,TKSTAR,TKPLUS,TKCMA,TKMINUS,TKDOT,TKFSL,
-	TKN,TKN,TKN,TKN,TKN,TKN,TKN,TKN,TKN,TKN,TKCOL,TKSCOL,TKLT,TKEQ,TKGT,0,
-	0,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,
-	TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,0,0,0,0,TKL,
-	0,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,
-	TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,TKL,0,0,0,0,0,
+	26,4,4,4,4,4,4,4,4,4,TKCOL,TKCOMMENT,TKLT,TKEQ,TKGT,0,
+	0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,3,
+	0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,
+},
+{
+	0,0,0,0,0,0,0,0,0,0,TKEOL,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	TKWS,TKBANG,TKDQ,TKHASH,TKDS,TKPCS,TKAMP,TKSQ,
+	TKLP,TKRP,TKSTAR,TKPLUS,TKCMA,TKMINUS,TKDOT,TKFSL,
+	31,31,31,31,31,31,31,31,0,0,TKCOL,TKCOMMENT,TKLT,TKEQ,TKGT,0,
+	0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,27,0,0,0,0,0,0,0,
+	0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,27,0,0,0,0,0,0,0,
+},
+{
+	0,0,0,0,0,0,0,0,0,0,TKEOL,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	TKWS,TKBANG,TKDQ,TKHASH,TKDS,TKPCS,TKAMP,TKSQ,
+	TKLP,TKRP,TKSTAR,TKPLUS,TKCMA,TKMINUS,28,TKFSL,
+	28,28,28,28,28,28,28,28,28,28,29,TKCOMMENT,TKLT,TKEQ,TKGT,0,
+	0,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,
+	28,28,28,28,28,28,28,28,28,28,28,0,0,0,0,28,
+	0,28,28,28,28,28,28,28,28,28,28,28,28,28,28,28,
+	28,28,28,28,28,28,28,28,28,28,28,0,0,0,0,0,
+},
+{
+	0,0,0,0,0,0,0,0,0,0,TKEOL,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	TKWS,TKBANG,TKDQ,TKHASH,TKDS,TKPCS,TKAMP,TKSQ,
+	TKLP,TKRP,TKSTAR,TKPLUS,TKCMA,TKMINUS,TKDOT,TKFSL,
+	30,30,30,30,30,30,30,30,30,30,TKCOL,TKCOMMENT,TKLT,TKEQ,TKGT,0,
+	0,30,30,30,30,30,30,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,30,30,30,30,30,30,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+}
 };
+
+struct lex_ctl {
+	int nmo; // the new mode
+	int nlc; // which table (above) to use next
+	int tkn; // what token to make
+	int mtkn; // -2 continuation, -1 clear token, >0 override token
+};
+
+// here be dragons
+static struct lex_ctl wvtr[] = {
+	{0, 0, 0},
+	{1, 0, TKWS, -2},
+	{0, 0, TKHASH},
+	{28, 2, TKL},
+	{30, 3, TKDEC, -2},
+	{0, 0, TKBANG}, // 5
+	{6, 0, TKDQ},
+	{7, 0, TKDS},
+	{28, 2, TKPCS},
+	{0, 0, TKAMP},
+	{10, 0, TKSQ}, // 10
+	{0, 0, TKLP},
+	{0, 0, TKRP},
+	{0, 0, TKSTAR},
+	{0, 0, TKPLUS},
+	{0, 0, TKCMA}, // 15
+	{0, 0, TKMINUS},
+	{28, 2, TKDOT, -2},
+	{0, 0, TKFSL},
+	{28, 2, TKCOL, -2},
+	{25, 0, TKWS}, // 20
+	{0, 0, TKLT},
+	{0, 0, TKGT},
+	{0, 0, TKEQ},
+	{24, 0, TKEOL},
+	{25, 0, TKWS, -2}, // 25
+	{26, 1, TKDEC, -2}, // 26 leading zero 0nn oct, 0xnn hex, 0bnn bin
+	{30, 3, 0, TKHEX},
+	{28, 2, 0, -2},
+	{28, 2, 0, TKCOL},
+	{30, 3, 0, -2}, // 30
+	{30, 3, 0, TKOCT},
+	{30, 3, 0, TKBIN},
+};
+
+static void diag_color(const char * txt, size_t s, size_t e) {
+	fprintf(stderr, "\e[31m");
+	size_t x;
+	for(x = s; x < e; x++) {
+		fputc(txt[x], stderr);
+	}
+	fprintf(stderr, "\e[0m");
+}
 
 int wva_lex(void * wvas, char * text, size_t len) {
 	size_t i = 0;
 	char cc = 0;
 	int mo = 0;
+	int lc = 0;
+	int mtkn = 0;
 	int trsc;
+	size_t tbegin, tend;
+	const int psz = sizeof(wvtr) / sizeof(struct lex_ctl);
 	while(i <= len) {
 		cc = i == len ? 0 : text[i];
-		trsc = sttab[cc];
-		if(!trsc) {
-			fprintf(stderr, "Invalid char: %d [%c]\n", cc, cc);
-			return 1;
-		} else {
-			fprintf(stderr, "Mode: [%c]%d\n", cc, trsc);
+		switch(mo) {
+		case TKCOMMENT:
+			if(cc == 10 || cc == 13) {
+				mo = 24;
+				fputc(10, stderr);
+				mtkn = TKEOL;
+			}
+			break;
+		default:
+			trsc = sttab[lc][cc];
+			if(!trsc) {
+				fprintf(stderr, "Invalid char: %d [%c]\n", cc, cc);
+				return 1;
+			} else {
+				if(trsc < psz) {
+					int nmo = wvtr[trsc].nmo;
+					lc = wvtr[trsc].nlc;
+					int nxtkn = wvtr[trsc].mtkn;
+					int ntkn = wvtr[trsc].tkn;
+					if(nxtkn > 0) {
+						mtkn = nxtkn;
+					} else if(nxtkn == -1) {
+						mtkn = 0;
+					}
+					if(nmo != mo) {
+						if(!ntkn && nxtkn > 0) {
+						} else {
+							if(mtkn && (nxtkn != -2 || mtkn != ntkn)) {
+								fprintf(stderr, "{%d}", mtkn);
+								tend = i;
+								diag_color(text, tbegin, tend);
+							}
+							if(mtkn != ntkn) tbegin = i;
+							mtkn = ntkn;
+						}
+						fprintf(stderr, "[%d]%c", nmo, cc);
+						mo = nmo;
+					} else {
+						if(mtkn && (nxtkn == 0) && ntkn) {
+							fprintf(stderr, "{|%d}", mtkn);
+							tend = i;
+							diag_color(text, tbegin, tend);
+							tbegin = i;
+							mtkn = ntkn;
+						}
+						fputc(cc, stderr);
+					}
+				} else {
+					fprintf(stderr, "lex: Mode out of range\n");
+					return -1;
+				}
+			}
+			break;
 		}
 		i++;
 		if(i >= len) {
