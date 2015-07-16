490e118b517e1cecae04a89083497486caef4cfa
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2015-07-17 03:30:59 +0900

    shoved the c++ hash impl into wave.cpp, added c++11 options

--- libwaveasm/hash.h
+++ libwaveasm/hash.h
@@ -13,25 +13,5 @@
 WAVEASMC
 uint32_t wva_murmur3(const void * key, size_t len);
 
-struct wvas_hashentry {
-	uint32_t keylen;
-	char * key;
-	uint32_t datalen;
-	void * data;
-	struct wvas_hashentry * next;
-};
-
-typedef struct wvas_arrayhash {
-	uint32_t tablesize;
-	uint32_t usedslots;
-	uint32_t entries;
-	struct wvas_hashentry ** table;
-} *wvat_hashtab;
-
-struct wvas_hashentry* wva_lookuphash(wvat_hashtab, const char *, uint32_t);
-int wva_addhash(wvat_hashtab, const char *, uint32_t, const uint8_t *, uint32_t);
-int wva_allochash(wvat_hashtab*);
-void wva_freehash(wvat_hashtab);
-
 #endif
 
--- libwaveasm/wave.cpp
+++ libwaveasm/wave.cpp
@@ -22,6 +22,9 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+#include <string>
 
 int strneq(const char *a, size_t al, const char *b, size_t bl)
 {
@@ -29,10 +32,200 @@ int strneq(const char *a, size_t al, const char *b, size_t bl)
 	return strncmp(a, b, al) == 0;
 }
 
-typedef struct wvai_state {
+// MurmurHash3 was written by Austin Appleby, and is public domain.
+// see http://code.google.com/p/smhasher/
+#define ROTL32(x,y) ((x << y) | (x >> (32 - y)))
+extern "C"
+uint32_t wva_murmur3(const void * key, size_t len)
+{
+	const uint8_t * data = (const uint8_t*)key;
+	const size_t nblocks = len / 4;
+
+	uint32_t h1 = 0;
+
+	const uint32_t c1 = 0xcc9e2d51;
+	const uint32_t c2 = 0x1b873593;
+
+	//----------
+	// body
+
+	const uint32_t * blocks = (const uint32_t *)(data);
+
+	size_t i;
+	for(i = nblocks; i; i--) {
+		uint32_t k1 = *(blocks++);
+
+		k1 *= c1;
+		k1 = ROTL32(k1,15);
+		k1 *= c2;
+
+		h1 ^= k1;
+		h1 = ROTL32(h1,13);
+		h1 = h1 * 5 + 0xe6546b64;
+	}
+
+	//----------
+	// tail
+
+	const uint8_t * tail = (const uint8_t*)(blocks);
+
+	uint32_t k1 = 0;
+
+	switch(len & 3) {
+	case 3: k1 ^= tail[2] << 16;
+	case 2: k1 ^= tail[1] << 8;
+	case 1: k1 ^= tail[0];
+		k1 *= c1;
+		k1 = ROTL32(k1,15);
+		k1 *= c2;
+		h1 ^= k1;
+	}
+
+	//----------
+	// finalization
+
+	h1 ^= len;
+
+	h1 ^= h1 >> 16;
+	h1 *= 0x85ebca6b;
+	h1 ^= h1 >> 13;
+	h1 *= 0xc2b2ae35;
+	h1 ^= h1 >> 16;
+
+	return h1;
+}
+
+namespace Wave {
+
+struct hashentry {
+	uint32_t keylen;
+	char *key;
+	uint32_t datalen;
+	void *data;
+	hashentry * next;
+};
+
+struct hashentry* wva_getentryhash(struct hashentry* hte, const char * key, uint32_t kl) {
+	while(hte) {
+		if(hte->keylen == kl && strncmp(key, hte->key, kl) == 0) {
+			return hte;
+		}
+		hte = hte->next;
+	}
+	return 0;
+}
+
+template<typename T>
+class arrayhash {
+private:
+	uint32_t tablesize;
+	uint32_t usedslots;
+	uint32_t entries;
+	hashentry ** table;
+public:
+	hashentry* lookup(const char *key, uint32_t kl) { 
+		uint32_t h;
+
+		if(!this) return 0;
+		h = wva_murmur3(key, kl) % this->tablesize;
+		fprintf(stderr, "HT: H:%d P:%X\n", h, this->table[h]);
+		return wva_getentryhash(this->table[h], key, kl);
+	}
+	int set(const char * key, uint32_t kl, const T &d) {
+		uint32_t h;
+		uint8_t *td;
+		hashentry* hte;
+		hashentry* htn;
+
+		if(!this || !key || !kl) return 0;
+		h = wva_murmur3(key, kl) % this->tablesize;
+		hte = this->table[h];
+		if(!hte) {
+			hte = (struct hashentry*)wva_alloc(sizeof(struct hashentry));
+			if(!hte) return 4;
+			hte->next = 0;
+			hte->key = (char*)wva_alloc(kl);
+			if(!hte->key) {
+				wva_free(hte);
+				return 4;
+			}
+			hte->keylen = kl;
+			hte->datalen = 0;
+			hte->data = 0;
+			memcpy(hte->key, key, kl);
+			this->table[h] = hte;
+		} else {
+			htn = wva_getentryhash(hte, key, kl);
+			if(htn) {
+				hte = htn;
+			} else {
+				htn = hte;
+				hte = (struct hashentry*)wva_alloc(sizeof(struct hashentry));
+				if(!hte) return 4;
+				hte->next = htn;
+				hte->key = (char*)wva_alloc(kl);
+				if(!hte->key) {
+					wva_free(hte);
+					return 4;
+				}
+				hte->keylen = kl;
+				hte->datalen = 0;
+				hte->data = 0;
+				memcpy(hte->key, key, kl);
+				this->table[h] = hte;
+			}
+		}
+		if(!hte->datalen) {
+			hte->data = new T(d);
+			hte->datalen = sizeof(T);
+		} else {
+			delete (T*)hte->data;
+			hte->data = new T(d);
+			hte->datalen = sizeof(T);
+		}
+		return 0;
+	}
+	arrayhash() {
+		void *arr;
+
+		this->tablesize = 63;
+		this->usedslots = 0;
+		this->entries = 0;
+		arr = wva_alloc(sizeof(void*) * this->tablesize);
+		if(!arr) {
+			return;
+		}
+		int i;
+		for(i = 0; i < this->tablesize; i++) {
+			((void**)arr)[i] = 0;
+		}
+		this->table = (struct hashentry**)arr;
+	}
+	~arrayhash() {
+		if(!this || !this->table) return;
+		int i;
+		struct hashentry* hte;
+		struct hashentry* htn;
+		for(i = 0; i < this->tablesize; i++) {
+			hte = this->table[i];
+			while(hte) {
+				if(hte->key) wva_free(hte->key);
+				if(hte->data) delete (T*)hte->data;
+				htn = hte->next;
+				wva_free(hte);
+				hte = htn;
+			}
+		}
+		wva_free(this->table);
+	}
+};
+
+} // namespace Wave
+
+struct wvai_state {
 	uint32_t nplatforms;
 	uint32_t nls;
-} *wvai_state;
+};
 
 int wva_allocstate(wvat_state *st)
 {
@@ -63,9 +256,8 @@ int wva_loadisf(wvat_state st, char *isftxt, size_t isflen)
 	x = 0; mode = 0;
 	ll = 0;
 	ln = 0;
-	wvat_hashtab ht;
-	struct wvas_hashentry *hte;
-	wva_allochash(&ht);
+	Wave::arrayhash<std::string> ht;
+	Wave::hashentry *hte;
 	while(x < isflen) {
 		ln++;
 		line = isftxt + x;
@@ -91,7 +283,7 @@ int wva_loadisf(wvat_state st, char *isftxt, size_t isflen)
 				mode = 0;
 				break;
 			}
-			if(0 != (rc = memchr(line, ':', ll))) {
+			if(0 != (rc = (const char*)memchr(line, ':', ll))) {
 				if(rc - line > MAXOPCODELEN) {
 					fprintf(stderr, "ISF: Opcode too long.\n");
 					break;
@@ -99,14 +291,15 @@ int wva_loadisf(wvat_state st, char *isftxt, size_t isflen)
 				memtolower(opc, line, rc-line);
 				opc[rc-line+1] = 0;
 				fprintf(stderr, "ISF: Opcode: %s\n", opc);
-				hte = wva_lookuphash(ht, opc, rc-line);
+				hte = ht.lookup(opc, rc-line);
 				if(!hte) {
 					fprintf(stderr, "ISF: Adding opcode\n");
-					wva_addhash(ht, opc, rc-line, rc, ll - (rc-line));
+					ht.set(opc, rc-line, std::string(rc, ll - (rc-line)));
 				} else {
 					fprintf(stderr, "ISF: Adding encoding\n");
-					wva_addhash(ht, opc, rc-line, rc, ll - (rc-line));
-					write(2, hte->data, hte->datalen);
+					std::string &st = *(std::string*)hte->data;
+					st.append(rc, ll - (rc-line));
+					fprintf(stderr, "ISF: %s\n", st.c_str());
 				}
 			}
 			break;
@@ -116,7 +309,6 @@ int wva_loadisf(wvat_state st, char *isftxt, size_t isflen)
 		}
 		x = e + 1;
 	}
-	wva_freehash(ht);
 	return 0;
 }
 
